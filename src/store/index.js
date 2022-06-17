62241031e1976dc90600cd48733eb3f1e4cc6dec
i.sharovatov <i.sharovatov@dunice.net>
2022-06-17 10:49:02 +0300

    add store

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga'
+
+import reducers from './reducer'
+import mySaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = configureStore(
+  {
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+  }
+)
+
+sagaMiddleware.run(mySaga)
+
+export default store
