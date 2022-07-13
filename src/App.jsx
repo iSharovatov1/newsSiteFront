6460b24320bee4a1001b3dadd330b741dd029b99
Kolovrat566 <sharobatov2001@mail.ru>
2022-07-13 18:00:04 +0300

    fix structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import getNews from './store/actions';
-import News from './components/News';
+import News from './components/News/News';
 
-import styles from './App.module.scss';
+import { getNews } from './store/action/newsActions';
 
 function App() {
   const dispatch = useDispatch();
   const { news, loading, error } = useSelector((state) => state);
 
   useEffect(() => {
-    dispatch(getNews);
+    dispatch(getNews());
   }, []);
 
   return (
-    <div className={styles.app}>
-      <div className={styles.title}>News</div>
-      <News news={news} loading={loading} error={error} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<News news={news} loading={loading} error={error} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
